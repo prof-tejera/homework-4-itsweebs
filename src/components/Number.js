34672db71581654f021d9e7b2c8453ac26b88f87
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-10-11 23:56:32 -0400

    remove provided styling

--- src/components/Number.js
+++ src/components/Number.js
@@ -2,11 +2,6 @@ const Number = ({ value, onClick }) => {
   //pass number value to calculator
   return (
     <div
-      style={{
-        padding: 10,
-        border: "1px solid black",
-        width: 60,
-      }}
       onClick = {() => onClick(value)}
     >
       {value}
--- src/components/Operation.js
+++ src/components/Operation.js
@@ -2,11 +2,6 @@ const Operation = ({ value, onClick }) => {
   //pass operation value to calculator
   return (
     <div
-      style={{
-        padding: 10,
-        border: "1px solid black",
-        width: 60,
-      }}
       onClick = {() => onClick(value)}
     >
       {value}
